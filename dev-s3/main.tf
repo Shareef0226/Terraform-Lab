672dbc83a28ab3dd5a88d80beae5720fe62ad57f
Shareef0226 <142314145+Shareef0226@users.noreply.github.com>
2024-05-08 14:52:46 +0530

    Delete dev-s3 directory

--- dev-s3/main.tf
+++
@@ -1,7 +0,0 @@
-module "dev-s3" {
-  source     = "../modules/s3"
-  s3_buckets = var.s3_buckets
-  tags       = var.tags
-  versioning = var.versioning
-  acl        = var.s3_acl
-}
--- dev-s3/variables.auto.tfvars
+++
@@ -1,7 +0,0 @@
-###
-s3_buckets = "dev-s3-bucket-east-1"
-
-tags = {
-  "Environment" = "Dev"
-}
-
